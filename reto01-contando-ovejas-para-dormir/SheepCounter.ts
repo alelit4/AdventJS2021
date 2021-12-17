f0890d97158fd2c47c59708ad72ce709efc96501
Ivan <Ivansantosgonz@gmail.com>
2021-12-17 20:52:49 +0000

    GREEN: SheepCounter should return only red sheep and name that contains letter "n"

--- reto01-contando-ovejas-para-dormir/SheepCounter.ts
+++ reto01-contando-ovejas-para-dormir/SheepCounter.ts
@@ -1,3 +1,6 @@
-const sheepCounter = (sheep) => sheep.filter((aSheep) => aSheep.color === 'rojo');
+const sheepCounter = (sheep) => {
+  const redSheep = sheep.filter((aSheep) => aSheep.color === 'rojo');
+  return redSheep.filter((aRedSheep) => aRedSheep.name.includes('n') || aRedSheep.name.includes('N'));
+};
 
 export default sheepCounter;

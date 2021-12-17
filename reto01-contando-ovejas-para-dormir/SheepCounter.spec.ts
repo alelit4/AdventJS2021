d77c49da8f3a0f651eeba1efe209290be3f8ec8b
Ivan <Ivansantosgonz@gmail.com>
2021-12-17 20:49:26 +0000

    RED: SheepCounter should return only red sheep and name that contains letter "n"

--- reto01-contando-ovejas-para-dormir/SheepCounter.spec.ts
+++ reto01-contando-ovejas-para-dormir/SheepCounter.spec.ts
@@ -10,17 +10,15 @@ describe('SheepCounter', () => {
     { name: 'Nnnnnnnn', color: 'rojo' },
   ];
 
-  const redSheep = [
-    { name: 'Euge', color: 'rojo' },
+  const redSheepWithN = [
     { name: 'Navidad', color: 'rojo' },
     { name: 'Ki Na Ma', color: 'rojo' },
-    { name: 'AAAAAaaaaa', color: 'rojo' },
     { name: 'Nnnnnnnn', color: 'rojo' },
   ];
 
-  it('should return only red sheep', () => {
+  it('should return only red sheep and name that contains letter "n"', () => {
     const filteredSheep = sheepCounter(sheep);
 
-    expect(filteredSheep).toStrictEqual(redSheep);
+    expect(filteredSheep).toStrictEqual(redSheepWithN);
   });
 });
